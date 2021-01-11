2b60b8ff4502e7c27777a90d7800348e2bd0b850
JonathanOzsvath <ozsvathjonathan@gmail.com>
2021-01-11 12:23:19 +0100

    Added allowed host

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -25,8 +24,10 @@ SECRET_KEY = '(t&48fj%_3&n9^22f#k3g!t_wm_#%4-$c%vnz+3@l$rx1$30be'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['ec2-18-191-1-181.us-east-2.compute.amazonaws.com',
+                 'localhost',
+                 '127.0.0.1'
+                 ]
 
 # Application definition
 
@@ -74,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'profiles_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -84,7 +84,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -104,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -118,11 +116,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
 
-AUTH_USER_MODEL= 'profiles_api.UserProfile'
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
